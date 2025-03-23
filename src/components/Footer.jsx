8c5be91acc31e56f76cedc4655ec06f0c53c08ee
Gabe <184721807+gabyel-dev@users.noreply.github.com>
2025-03-23 15:22:22 +0800

    final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,29 @@
 import "./css/Base.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faFacebook,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
+const github = <FontAwesomeIcon icon={faGithub} className="cursor-pointer" />;
+const facebook = <FontAwesomeIcon icon={faFacebook} />;
+const linkedIn = <FontAwesomeIcon icon={faLinkedin} />;
 
 export default function Footer() {
   return (
-    <div className="bg-[#6d83f2] w-full h-[50px] bottom-0 left-0 justify-between items-center flex pl-[10vw] pr-[10vw] text-white mont">
-      <img src="/logo-footer.jpg" alt="logo" className="w-9 h-9 rounded-md" />
-      <p className="text-sm">Made by Gabyel</p>
+    <div className="bg-[#6c85ff] pt-20 pb-20  w-full h-[250px] bottom-0 left-0 justify-center items-center flex-col gap-[2vh] flex  pl-[10vw] pr-[10vw] text-white mont">
+      <h1 className="text-3xl font-bold ">Gabriel Guban</h1>
+      <p className="text-sm text-center italic">
+        You can visit my social media and follow me. Just click the social media
+        symbols below. Thank You!
+      </p>
+      <div className="text-2xl flex gap-[3vh] pt-3">
+        {github}
+        {facebook}
+        {linkedIn}
+      </div>
+      <p className="text-sm text-center pt-6">All Rights Reserved</p>
     </div>
   );
 }
